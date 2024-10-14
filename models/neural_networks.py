7a97a8fdad5f70e3893985841ee84d524000d36f
fullo <fulignimatteo@gmail.com>
2024-10-14 23:14:22 +0200

    started neural networks class

--- models/neural_networks.py
+++ models/neural_networks.py
@@ -1,1 +1,14 @@
 # Definizione della rete neurale (costruzione e forward pass)
+
+import tensorflow as tf 
+from keras import layers, models 
+
+class NeuralNewtwok:
+    def __init__(self, input_size, output_size, hidden_layers=[32, 32]):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_layers = hidden_layers
+        
+        self.model = self.build_model(self.input_size, self.output_size, self.hidden_layers)
+        
+    
